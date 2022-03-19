c61568be3166e8e1fb7107d3e65e319ba02423be
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-03-19 01:19:30 +0100

    update: docker

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Iterations - MS Monitoring
 
-TODO: logs
+## Documentation
+
+See `its__documentation` for more information ([github](https://github.com/iterations-is/its__documentation)).
+
+## Docker build
+
+- Create an `.env.production` file
+- Run `yarn docker:build` from this directory or docker-compose from `its` directory (it you use repository with submodules).
--- src/constants.ts
+++ src/constants.ts
@@ -3,12 +3,12 @@ export const MS_EXPRESS_PORT = process.env.MS_EXPRESS_PORT ?? 3000;
 export const BROKER_URL = process.env.BROKER_URL ?? 'amqp://localhost';
 export const MONGO_URL = process.env.MONGO_URL ?? 'mongodb://localhost:27017';
 
-export const ITS_MS_AUTH = process.env.ITS_MS_AUTH;
-export const ITS_MS_COMMUNICATION = process.env.ITS_MS_COMMUNICATION;
-export const ITS_MS_INTERPRETERS = process.env.ITS_MS_INTERPRETERS;
-export const ITS_MS_MONITORING = process.env.ITS_MS_MONITORING;
-export const ITS_MS_PROJECTS = process.env.ITS_MS_PROJECTS;
-export const ITS_MS_USERS = process.env.ITS_MS_USERS;
+export const ITS_MS_AUTH = process.env.URI_MS_AUTH;
+export const ITS_MS_COMMUNICATION = process.env.URI_MS_COMMUNICATION;
+export const ITS_MS_INTERPRETERS = process.env.URI_MS_INTERPRETERS;
+export const ITS_MS_MONITORING = process.env.URI_MS_MONITORING;
+export const ITS_MS_PROJECTS = process.env.URI_MS_PROJECTS;
+export const ITS_MS_USERS = process.env.URI_MS_USERS;
 
 export const ITS_MS_PATHS = [
 	{ name: 'AUTH', baseUrl: ITS_MS_AUTH },
