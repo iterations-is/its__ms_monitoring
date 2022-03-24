96ddbf5bd5db77a5679ff3383e56ae3f9e7d8d73
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-03-24 01:16:32 +0100

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import 'dotenv/config';
 import express, { json } from 'express';
+import cors from 'cors';
 
 import { aboutRouter, runBroker } from '../src-ms';
 import { MS_EXPRESS_PORT } from './constants';
 import { externalRouter, internalRouter } from './routes';
 import { channelConsumers, channelProducers } from './broker';
 
-//RMQ
+// RMQ
 runBroker(channelProducers, channelConsumers);
 
 // Express
 const app = express();
 
+app.use(cors());
 app.use(json());
 
 app.use('/monitoring-service', aboutRouter);
